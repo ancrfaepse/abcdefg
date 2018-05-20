072e21da0121f43127942469cdaa56f699622b12
kkimura <qp5wgold@gmail.com>
2018-05-20 22:32:56 +0900

    go処理の実験

--- hello.go
+++ hello.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	fmt.Println("処理開始")
+	fmt.Println("通常処理コール")
+
+	fmt.Println("通常コール")
+	serialno()
+
+	fmt.Println("並列処理コール")
+	go serialno()
+	time.Sleep(1 * time.Second)
+
+	fmt.Println("処理終了")
+
+}
+
+func serialno() {
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
 }
